ed28460208261cdf8426dd30aeb3a586a70ef1c6
Raisson Arbusti Paludo <raissonarbustepaludo@gmail.com>
2025-10-02 10:33:20 -0300

    att gitignore e adiconado mais rotas

--- web_system/relacionamentos/urls.py
+++ web_system/relacionamentos/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-import relacionamentos.views as views
+import relacionamentos.views as views_funcoes
 
 app_name = 'relacionamentos'
 
 urlpatterns = [
-    path('funcao/teste', views.primeira_view,
-         name='primeira_view')
+    path('funcao/teste', views_funcoes.primeira_view,
+         name='primeira_view'),
+    
+    path('funcao/saudacao', views_funcoes.saudacao,
+         name="saudacao"),
+    
+    # Qualquer coisa em string, deve sempre ser por ultimo para nao inutilizar as urls de cima que sao fixas
+    # nome é a variavel que vai armazenar o que vem depois de funcao/
+    path('funcao/<str:nome>', views_funcoes.nome,
+         name="nome")
 ]
--- web_system/relacionamentos/views/funcoes.py
+++ web_system/relacionamentos/views/funcoes.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, redirect, HttpResponse
-# from django.core import serializers
-# from django.http import JsonResponse
-# from django.core.serializers import serialize
-# from datetime import datetime
-# from django.views import View
+from django.core import serializers
+from django.http import JsonResponse
+from django.core.serializers import serialize
+from datetime import datetime
+from django.views import View
+from relacionamentos.models.reporter import Reporter
 
 def primeira_view(request):
     mensagem = "Bom dia"
     return HttpResponse(mensagem, status=200)
+
+def saudacao(request):
+    agora = datetime.now()
+    mensagem = "boa noite"
+    if 12 > agora.hour > 6:
+        mensagem ="bom dia"
+    elif 0 < agora.hour <=6:
+        mensagem = "boa madrugada"
+
+    completo = f"<html><body><h1>{mensagem.capitalize()}  visitante!<br /> {agora}</h1>{request.META["REMOTE_ADDR"]}</body></html>"
+    return HttpResponse(completo)
+
+def nome(request, nome):
+    reporter = Reporter.objects.find_by_name(nome)
+    objeto = serializers.serialize('python', reporter)
+    return JsonResponse(objeto, safe=False)
+    #return HttpResponse(reporter, status=200)
