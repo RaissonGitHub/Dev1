6d644ab8301692a745e58c0a41bd83e6e34803ee
Raisson Arbusti Paludo <raissonarbustepaludo@gmail.com>
2025-10-09 11:49:51 -0300

    menu template

--- web_system/web_system/urls.py
+++ web_system/web_system/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from web_system.views import estaticas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('relacionamentos/', include('relacionamentos.urls'),)
+    path('relacionamentos/', include('relacionamentos.urls')),
+    path('', estaticas.index, name="index")
 ]
